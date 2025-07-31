b97f5d4b291a8afcd957022ea205167cad897645
M. Gilang Chandrawinata <152847757+MGilangChandra@users.noreply.github.com>
2025-07-31 14:08:19 +0700

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
-export default {
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            textStrokeWidth: {
+                DEFAULT: '1px',
+                '2': '2px',
+            },
+            textStrokeColor: {
+                black: '#000',
+                white: '#fff',
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        function ({ addUtilities }) {
+            addUtilities({
+                '.text-stroke': {
+                    '-webkit-text-stroke': '1px black',
+                },
+                '.text-stroke-white': {
+                    '-webkit-text-stroke': '1px white',
+                },
+                '.text-stroke-2': {
+                    '-webkit-text-stroke': '2px black',
+                },
+            })
+        },
+    ],
 }
